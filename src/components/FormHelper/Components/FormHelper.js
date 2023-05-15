0405296f20fd06bb85576870094254b2e70e6b1b
Nathan Bate <nathan@clearfirestudios.com>
2023-05-14 21:16:56 -0400

    Fix: Add file extensions

--- src/components/FormHelper/Components/FormHelper.js
+++ src/components/FormHelper/Components/FormHelper.js
@@ -1,5 +1,5 @@
-import FormLayout from './FormHelper/FormHelper';
-import Row from './FormHelper/FormRow'
-import { LightSwitch, Number, Select, Text } from './Fields'
+import FormLayout from './FormHelper/FormHelper.vue';
+import Row from './FormHelper/FormRow.vue'
+import { LightSwitch, Number, Select, Text } from './Fields.js'
 
 export { FormLayout, Row, LightSwitch, Number, Select, Text }
